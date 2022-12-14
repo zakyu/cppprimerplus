c9dde851eaf790fd186c3a6e2f8202af9f931929
zakyu <zekaiyu.97@gmail.com>
2022-12-15 03:09:21 +0800

    update

---
+++ chapter02/myfirst.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+   printf("Hello in c.\n"); // C 
+
+    std::cout << "Come up and C++ me some time.";
+    std::cout << std::endl;
+    std::cout << "You won't regret it!" << std::endl;
+
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+
+    return 0;
+}
